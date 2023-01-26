851bcd8a04dfdc63335baf206bff36c9c08070eb
hpweisberg <hpweisberg@gmail.com>
2023-01-25 16:36:50 -0800

    create nav bar and burgershop link

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
+import { Route, Routes } from 'react-router-dom'
 import React from 'react'
 import './styles/App.css'
+import Nav from './Components/Nav/Nav'
+
+import BurgerShop from './Components/BurgerShop/BurgerShop'
 
 // Components
 import Landing from './Components/Landing/Landing'
@@ -7,7 +11,11 @@ import Landing from './Components/Landing/Landing'
 const App = () => {
   return (
     <main>
+      <Nav></Nav>
       <Landing />
+      <Routes>
+        <Route path='/burgers' element={<BurgerShop />} />
+      </Routes>
     </main>
   )
 }
--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,10 +1,12 @@
 // Components & Assets
 import Logo from '../../assets/react-logo.png'
+import { NavLink } from 'react-router-dom'
 
 const Nav = () => {
   return (
     <nav className="navigation-bar">
-
+      <NavLink to='/' id='logo'><img src={Logo} alt="" /></NavLink>
+      <NavLink to='/burgers'>BURGER SHOP</NavLink>
     </nav>
   )
 }
